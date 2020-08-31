425a7e40a099feb6674f456782f8a35d595394e8
Aaron Tobey <abtobey@gmail.com>
2020-08-31 13:19:05 -0500

    fixed lint formating issues

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,11 +1,11 @@
-var express = require("express");
+var express = require('express');
 
 var router = express.Router();
 
-var movie = require("../models/movie.js");
+// var movie = require('../models/movie.js');
 
-router.get('/test', function(req, res){
-    res.render("index")
-  });
+router.get('/test', function (req, res) {
+  res.render('index');
+});
 
 module.exports = router;
--- models/movie.js
+++ models/movie.js
@@ -1,45 +1,45 @@
-module.exports = function(sequelize, DataTypes) {
-  const Movie = sequelize.define("Movie", {
+module.exports = function (sequelize, DataTypes) {
+  const Movie = sequelize.define('Movie', {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
-      },
+        len: [1]
+      }
     },
     streamService: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
-      },
+        len: [1]
+      }
     },
     rating: {
       type: DataTypes.FLOAT,
       allowNull: false,
       validate: {
         max: 10,
-        min: 0,
-      },
+        min: 0
+      }
     },
     writeUp: {
       type: DataTypes.STRING,
       allowNull: true,
       validate: {
-        len: [0, 240],
-      },
+        len: [0, 240]
+      }
     },
     recommend: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
+      allowNull: false
+    }
   });
 
-  Movie.associate = function(models) {
+  Movie.associate = function (models) {
     Movie.belongsTo(models.User, {
       foreignKey: {
-        allowNull: false,
-      },
+        allowNull: false
+      }
     });
   };
 
