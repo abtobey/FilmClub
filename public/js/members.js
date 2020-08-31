a12618beb585816b1b99a0ef7be5753fda1f2b25
Ian Pyeatt <pyeatt08@gmail.com>
2020-08-31 13:44:51 -0500

    created object with user input

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,26 @@
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get('/api/user_data').then(data => {
+  $.get('/api/user_data').then((data) => {
     $('.member-name').text(data.email);
+  });
+  const title = $('#movieTitle');
+  const rating = $('#rating');
+  const writeUp = $('#writeUp');
+  const streaming = $('#streamingService');
+
+  $('#submitButton').on('click', function () {
+    event.preventDefault();
+
+    const newMovie = {
+      title: title.val().trim(),
+      rating: rating.val() / 10,
+      writeUp: writeUp.val().trim(),
+      streaming: streaming.val().trim(),
+      recommend: $('[name=recommend]:checked').val().trim()
+
+      // userName: userInput.val().trim()
+    };
+    console.log(newMovie);
   });
 });
