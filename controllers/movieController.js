5496fb2a3accaec5db23d45782e8551e49299830
Ian Pyeatt <pyeatt08@gmail.com>
2020-09-02 18:55:44 -0500

    corrected typo in controllers folder

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,24 +1,22 @@
-var express = require('express');
+var express = require("express");
 // const db = require('../models');
 
 var router = express.Router();
 
 // var movie = require('../models/movie.js');
 // const db = require('../models');
-module.exports = function (app) {
-
+module.exports = function(app) {
   // router.post('/api/movie', (req, res) => {
-
-//   db.Movie.create({
-//     title: req.body.title,
-//     streamService: req.body.streaming,
-//     rating: req.body.rating,
-//     writeUp: req.body.writeUp,
-//     recommend: req.body.recommend,
-//     // need to figure out how to get the session user ID
-//     userId: 1
-//   });
-// });
+  //   db.Movie.create({
+  //     title: req.body.title,
+  //     streamService: req.body.streaming,
+  //     rating: req.body.rating,
+  //     writeUp: req.body.writeUp,
+  //     recommend: req.body.recommend,
+  //     // need to figure out how to get the session user ID
+  //     userId: 1
+  //   });
+  // });
 };
 
 module.exports = router;
